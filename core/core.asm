f3f218db89a6637c45ec31d89e31368288190099
zack123 <zerkshaban@live.com>
2015-11-05 18:06:01 +0500

    converted into struct array implementation

--- core/core.asm
+++ core/core.asm
@@ -1,117 +1,89 @@
 
-COORDv2 STRUCT
-	X BYTE ?
-	Y BYTE ?
-COORDv2 ENDS
+POINT STRUCT
+	x BYTE ?
+	y BYTE ?
+POINT ENDS
 
 .data
-    str1 BYTE "[======]",0
-    Block1x COORDv2 <?>
+	block BYTE "[======]",0
+	sizeA = 10
+	axis POINT sizeA DUP(<?,?>)
+	axis1 POINT sizeA DUP(<?,?>)
+	axis2 POINT sizeA DUP(<?,?>)
 	val1 BYTE 0
-	val2 BYTE 3
+	val2 BYTE 0
 	color_rand DWORD 2
-	Lives BYTE "Lives:3",0
-	Score BYTE "Score:0000",0
 .code 
 include helperPROC.asm
-z_LivesNscore PROC
-		
-		mov al,15
-		mov bl,0
-		mov Block1x.X, al
-		mov Block1x.Y, bl
-		
-		mov dl, Block1x.X
-		mov dh, Block1x.Y 
-		call Gotoxy   
-
-    	mov  eax,3 
-		call SetTextColor
-
-		mov edx,OFFSET Lives
-		call writestring
-		
-		mov al,55
-		mov bl,0
-		mov Block1x.X, al
-		mov Block1x.Y, bl
-		
-		mov dl, Block1x.X
-		mov dh, Block1x.Y 
-		call Gotoxy   
-
-    	mov  eax,3 
-		call SetTextColor
-
-		mov edx,OFFSET Score
-		call writestring
-		
-	ret
-z_LivesNscore ENDP
 core PROC
-		call z_LivesNscore	
-		mov ecx,10
-	L1:
-		
+	mov eax, 0
+	mov esi, 0
+	mov ecx, 10
+	row1:
 		mov al,val1
-		mov bl,val2
-		mov Block1x.X, al
-		mov Block1x.Y, bl
+		mov ah,val2
+		mov axis[esi].x,al 
+		mov axis[esi].y,ah
+	
+		mov dl, axis[esi].x
+		mov dh, axis[esi].y
+		call Gotoxy
 		
-		mov dl, Block1x.X
-		mov dh, Block1x.Y 
-		call Gotoxy   
-        mov  eax,color_rand 
+		mov eax,color_rand
 		call SetTextColor
-
-		mov edx,OFFSET str1
+		
+		mov edx,OFFSET block
 		call writestring
-		add val1,8
+		add val1,8	
 		add color_rand,1
-		loop L1
+	loop row1	
+		
 		mov color_rand,3
-		inc val2
+		add val2,1
 		mov ecx,10
 		
-	L2:
-		
+	row2:
 		mov al,val1
-		mov bl,val2
-		mov Block1x.X, al
-		mov Block1x.Y, bl
+		mov ah,val2
+		mov axis1[esi].x,al
+		mov axis1[esi].y,ah
 		
-		mov dl, Block1x.X
-		mov dh, Block1x.Y
+		mov dl,axis1[esi].x
+		mov dh,axis1[esi].y
 		call Gotoxy
-        mov  eax,color_rand
+		
+		mov eax,color_rand
 		call SetTextColor
-   
-		mov edx,OFFSET str1
+		
+		mov edx,OFFSET block
 		call writestring
 		add val1,8
 		add color_rand,1
-		loop L2
+	loop row2
+		
 		mov color_rand,4
-		inc val2
+		add val2,1
 		mov ecx,10
-	L3:
 		
+	row3:
 		mov al,val1
-		mov bl,val2
-		mov Block1x.X, al
-		mov Block1x.Y, bl
+		mov ah,val2
+		mov axis2[esi].x,al
+		mov axis2[esi].y,ah
 		
-		mov dl, Block1x.X
-		mov dh, Block1x.Y
-		call Gotoxy
-        mov  eax,color_rand
+		mov dl,axis2[esi].x
+		mov dh,axis2[esi].y
+		call Gotoxy 
+		
+		mov eax,color_rand
 		call SetTextColor
-
-		mov edx,OFFSET str1
+		
+		mov edx,OFFSET block
 		call writestring
 		add val1,8
 		add color_rand,1
-		loop L3
+	loop row3
+		
 		call crlf
 		call crlf
 		call crlf
