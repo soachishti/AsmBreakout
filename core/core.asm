0146ab79ca59b0af6e391585fdd5335587754a97
soachishti <soac@outlook.com>
2015-11-14 19:12:41 +0500

    Ball movement added.

--- core/core.asm
+++ core/core.asm
@@ -1,49 +1,71 @@
 VK_LEFT		EQU		000000025h
 VK_RIGHT	EQU		000000027h
+VK_ESCAPE	EQU		00000001bh
+VK_F1		EQU		000000070h
 maxCol      EQU     79
 maxRow      EQU     25
 
 GetKeyState PROTO, nVirtKey:DWORD
 
 POINT STRUCT
-	x BYTE ?
-	y BYTE ?
+	x       BYTE ?
+	y       BYTE ?
+    color   DWORD ? ; color
 POINT ENDS
 
 .data
-	stickCol        BYTE    0
-    stickRow        BYTE    25 ; 25 is standard
+    stickPos        POINT   <35, 25>           
+    
     stickSize       =       10
 	stick           BYTE    "<########>",0
 	Invisible_stick BYTE    "          ",0
-    maxBlock        =       30
+    maxBlock        =       30 + 10
     gameSpeed       =       25
-	block           BYTE    "[======]",0
+	;block           BYTE    "[======]",0
+	;block           BYTE    223,223,223,223,223,223,223,223,0
+	block           BYTE    219,219,219,219,219,219,219,219,0
 	Space           BYTE    "        ",0
-	sizeA           =       10
-	axis            POINT   sizeA DUP(<?,?>)
-	val1            BYTE    0
-	val2            BYTE    0
+	axis            POINT   maxBlock DUP(<?,?>)
 	color_rand      DWORD   2
+    
+    ballChar        BYTE    'o'
+    ball            POINT   <40,25>
+    ballDir         POINT   <1,1>
+    stickSpeed      =       3
+    score           DWORD   0
 .code 
 include helperPROC.asm
 
 eraseBlock PROC
-		mov al,val1
-		mov ah,val2
-		
-		mov axis[esi].x,al 
-		mov axis[esi].y,ah
 		mov dl, axis[esi].x
 		mov dh, axis[esi].y
 		call Gotoxy
 		mov edx,OFFSET Space
 		call writestring
-		
 	ret
 eraseBlock ENDP
 
-Print_Grid PROC
+printBlock PROC
+    	mov dl, axis[esi].x
+		mov dh, axis[esi].y
+		call Gotoxy
+		
+		mov eax, axis[esi].color
+		call SetTextColor
+				
+		mov edx,OFFSET block
+		call writestring
+    ret
+printBlock ENDP
+
+
+
+Init_Grid PROC
+        LOCAL val1:BYTE, val2:BYTE
+               
+        mov val1, 0
+        mov val2, 0
+
 		mov eax, 0
 		mov esi, 0
 		mov ecx, maxBlock
@@ -51,47 +73,55 @@ Print_Grid PROC
 		mov al,val1
 		mov ah,val2
 		
-		mov axis[esi].x,ah 
-		mov axis[esi].y,al
-	
-		mov dl, axis[esi].x
-		mov dh, axis[esi].y
-		call Gotoxy
-		
-		mov eax,color_rand
-		call SetTextColor
-				
-		mov edx,OFFSET block
-		call writestring
-		
+		mov axis[esi].x, ah 
+		mov axis[esi].y, al
+        
+        mov eax, color_rand
+        mov axis[esi].color, eax 
+        
+        call printBlock
+			
 	.if color_rand >= 15
 		mov color_rand, 3
 	.endif
 		
-	.if val2 >= 80
+        
+        add val2,8	
+		add color_rand,1
+	
+    .if val2 > maxCol
 		mov val2, 0
 		add val1, 1
 	.endif
-		add val2,8	
-		add color_rand,1	
+
+
+        add esi, TYPE axis
 	loop row1	
 
 ret
-Print_Grid ENDP
+Init_Grid ENDP
 
-StickMovement PROC
+StickMovement PROC    
         INVOKE GetKeyState, VK_LEFT
-        .IF ah && stickCol > 0 
-            DEC stickCol
+        .IF ah && stickPos.x > 0
+            mov al, stickSpeed
+            sub stickPos.x, al
+            .IF stickPos.x > maxCol
+                mov stickPos.x, 0 
+            .ENDIF
         .ENDIF  
 
         INVOKE GetKeyState, VK_RIGHT
-        .IF ah && stickCol <= maxCol - stickSize
-            INC stickCol
+        .IF ah && stickPos.x <= maxCol - stickSize
+            mov al, stickSpeed
+            add stickPos.x, al
+            .IF stickPos.x > maxCol - stickSize
+                mov stickPos.x, maxCol - stickSize + 1
+            .ENDIF
         .ENDIF     
-
-        mov  dl, stickCol        
-        mov  dh, stickRow        
+        
+        mov  dl, stickPos.x        
+        mov  dh, stickPos.y        
         call Gotoxy         
 
         mov eax,3    
@@ -99,25 +129,161 @@ StickMovement PROC
        
         mov  edx, OFFSET stick          
         call Writestring    
-        
-        invoke Sleep, gameSpeed
-        
-        mov  dl, stickCol        
-        mov  dh, stickRow        
-        call Gotoxy         
-        
-        mov  edx,OFFSET Invisible_stick     
-        call Writestring     
-        
-        add color_rand,1
 	ret
 StickMovement ENDP
 
+EraseStick PROC
+        mov  dl, stickPos.x        
+        mov  dh, stickPos.y        
+        call Gotoxy         
+        
+        mov  edx, OFFSET Invisible_stick     
+        call Writestring     
+        
+        add color_rand,1  
+    ret
+EraseStick ENDP
+
+BallMovement PROC
+    .IF ball.x >= maxCol   ; Right Wall
+        mov ballDir.x, -1
+  	.ENDIF
+
+    .IF ball.x <= 0        ; Left Wall
+        mov ballDir.x, 1
+	.ENDIF     
+
+    .IF ball.y <= 0        ; Top
+        mov ballDir.y, 1
+	.ENDIF  
+
+    .IF ball.y >= maxRow   ; Down limit
+        mov ballDir.y, -1
+	.ENDIF     
+
+    mov al, ballDir.x
+    add ball.x, al 
+    mov al, ballDir.y
+    add ball.y, al 
+    
+    mov  dl, ball.x        ; column
+    mov  dh, ball.y        ; row
+    call Gotoxy         ; Change position according to new input
+        
+    mov  al, ballChar          
+    call WriteChar      ; Write point on new place   
+    ret
+BallMovement ENDP
+
+EraseBall PROC
+    ; Erase Point
+    mov  dl, ball.x        ; column
+    mov  dh, ball.y        ; row
+    call Gotoxy         ; Change position according to new input
+    
+    mov  al,' '     
+    call WriteChar      ; Remove previous data
+    ret
+EraseBall ENDP
+
+stickcolision PROC
+     mov al, 0
+    .IF ball.y == maxRow
+        mov dl, stickPos.x 
+        mov dh, stickPos.x 
+        add dh, stickSize
+        .IF ball.x >= dl && ball.x <= dh
+            mov al, 0
+            ; control ball direction
+        .ELSE
+            mov al, 1
+        .ENDIF
+    .ENDIF
+    ret
+stickcolision ENDP
+
+Print_Block PROC
+    mov esi, 0
+	mov ecx, maxBlock
+    l0:
+        .IF axis[esi].x != -1 
+            call printBlock
+        .ENDIF
+        add esi, SIZEOF POINT
+    loop l0
+    ret
+Print_Block ENDP
+
+grid_collision_check PROC
+    mov esi, 0
+	mov ecx, maxBlock
+    l0:
+        mov al, ball.x
+        mov ah, ball.y
+        mov dl, axis[esi].x
+        add dl, 8
+        
+        .IF ah == axis[esi].y && al <= dl && al >= axis[esi].x 
+            call eraseBlock
+            inc score
+            
+            neg ballDir.x
+            neg ballDir.y
+            mov axis[esi].x, -1
+            mov axis[esi].y, -1
+            ret
+        .ENDIF
+        
+        add esi, SIZEOF POINT
+    loop l0
+    ret
+grid_collision_check ENDP
+
 core PROC
-    call Print_Grid
-  	foreverLoop: 	
-        call stickMovement		
+    call Init_Grid
+  	foreverLoop: 
+        INVOKE GetKeyState, VK_ESCAPE
+        .IF ah
+            jmp gamePause
+        .ENDIF
+	
+        call Print_Block
+        call stickMovement	
+        call BallMovement
+        
+        call grid_collision_check
+        
+        call stickcolision
+        cmp al, 1   ; collision 
+        je finish
+        
+        .IF score == maxBlock
+            jmp YouWin
+        .ENDIF
+        
+        invoke Sleep, gameSpeed
+
+        call EraseBall
+        call EraseStick
+        
+    resume:
         jmp foreverLoop
     
+    
+    YouWin:
+        
+        jmp return
+    restart:
+        mWrite "Game restart"
+        jmp return
+    finish:
+        ; Will add life
+        mWrite "Game End"
+        jmp return
+    gamePause:
+        mWrite "Game Paused"
+        jmp return
+        
+    return:
 	ret
 core ENDP
