6553875dfda36c2786fec392e8a6afc7548b6ef2
soachishti <soac@outlook.com>
2015-11-16 20:09:42 +0500

    Add main title

--- main.asm
+++ main.asm
@@ -1,16 +1,21 @@
 include Irvine32.inc
+include MACROS.inc
 
 ; Collecting team source
-include core\core.asm
-include ui\ui.asm
-include tests\tests.asm
+;include core\core.asm
+;include ui\ui.asm
+;include tests\tests.asm
 
+.data
+    titleStr    BYTE    "AsmBreakout v1.0", 0
 .code 
 main PROC
-   	call tests
-	call core
-	call ui
-	
+    invoke SetConsoleTitle, ADDR titleStr
+       
+   	;call tests
+	;call core
+	;call ui
+	call WaitMsg
 	exit
 main ENDP
 END main
